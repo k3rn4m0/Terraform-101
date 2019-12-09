5d620d39ebf4588c673a6e1af8eed84c98591660
vi-hseng <46452844+vi-hseng@users.noreply.github.com>
2019-12-09 15:47:42 -0500

    Directory with the main.tf file
    Includes everything your directory should look like ( also included the outputs to the assignment)

---
+++ AWS/main.tf
@@ -0,0 +1,115 @@
+#--------AWS KEY PAIR---------------------
+
+resource "aws_key_pair" "ec2_authn" {
+	public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDA8M/bm4eebIz2HEf3g7BCo+3DXRcyUDQVYMZr+lHY64S0iFwWDVyeN6FybXuJyVjjE0UnayBVxRvc2trO771TKb7Fk6YJYBzlzCCABqzuN3VyD6YH83Oba7YreeUuObkt6SvtIefieCK78KbitPzqGYq75z8zgCr0g3/1RIuLFyJ6skkBreWRqhi1WZTXKzslDMJFrDELw9Sju18TbTP0FpAKVKV6D4gCw+XO/GMlXgowFnIg0lF0roslaru7y/Qb7VtfWFSNo7I/eaJL2KEt8LzCBspaF2Wjorw/SvtKzGlLNyf9EuI09EEXv1y6aY6gl4QdcKLqmMfuAvTtma8Y0VWavW1iaW+Ls0Uwvv3xFGvHkfOJVWjDSwfot8j86MrVAUdvLZSzbBF5b+D3vac/v7EW8IDGAw5xN+bJZv4+qGaORKpqgpohv5AYA0HbM6LpPm2Pm1Ya28wx/zB4heRlohQ13jK1rolScVLKvkoXbt++UXEP4J3aOAzvPQYiqfc= vi-hseng@localhost.localdomain"
+	key_name   = "${var.key_name}"
+}
+
+#----------RESOURCE & PROVIDER BLOCKS------
+
+provider "aws" {
+  region = "${var.aws_region}"
+  shared_credentials_file = "/home/vi-hseng/.aws/credentials"
+}
+
+resource "aws_internet_gateway" "web-gateway" {
+  vpc_id = "${aws_vpc.web_server.id}"tags {
+    Name = "web-gateway"
+  }
+}
+
+resource "aws_vpc" "web_server" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  tags {
+    Name = "web_server"
+  }
+}
+
+resource "aws_subnet" "server-subnet-1" {
+  cidr_block = "${cidrsubnet(aws_vpc.web_server.cidr_block, 3, 1)}"
+  vpc_id = "${aws_vpc.web_server.id}"
+  availability_zone = "us-east-1a"
+}
+
+
+resource "aws_instance" "web_server" {
+  ami           = "${var.ami[var.aws_region]}"
+  instance_type = "t2.micro"
+  key_name = "terra_key"
+  security_groups = ["${aws_security_group.ie-sg.id}"]  
+  tags = {
+      Name = "Ubuntu-EC2-Server"
+    }
+  subnet_id = "${aws_subnet.server-subnet-1.id}"
+
+  user_data = "${file("user_data/nginx.sh")}"
+}
+
+resource "aws_eip" "web_server" {
+  instance = "${aws_instance.web_server.id}"
+  vpc      = true
+}
+
+#-------SECURITY GROUPS--------
+
+resource "aws_security_group" "ie-sg" {
+  name = "web-security-group"
+  vpc_id = "${aws_vpc.web_server.id}"
+  ingress {
+      cidr_blocks = ["0.0.0.0/0"]
+      from_port = 22
+      to_port = 22
+      protocol = "tcp"
+  }
+  
+  egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+ }
+}
+
+
+#------ROUTING TABLE---------
+
+resource "aws_route_table" "web_rt" {
+  vpc_id = "${aws_vpc.web_server.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.web-gateway.id}"
+  }tags {
+    Name = "web_rt"
+  }
+}
+
+resource "aws_route_table_association" "web-sub-association" {
+  subnet_id      = "${aws_subnet.server-subnet-1.id}"
+  route_table_id = "${aws_route_table.web_rt.id}"
+}
+
+#------VARIABLE BLOCK---------
+
+variable "key_name" {}
+variable "aws_region" {}
+
+variable "ami" {
+  type        = "map"
+  description = "The AMIs for Ubuntu to be used"
+
+  default = {
+    us-east-1 = "ami-04b9e92b5572fa0d1"
+    us-east-2 = "ami-0d5d9d301c853a04a"
+    us-west-1 = "ami-0dd655843c87b6930"
+    us-west-2 = "ami-06d51e91cea0dac8d"
+  }
+}
+
+#--------OUTPUTS BLOCK--------------
+
+output "ip_addresses" {
+  value = "${aws_eip.web_server.public_ip}"
+}
+
